0ed898fcf0df25ccc29e942dfb48feafe072b81c
Bartłomiej Kawa <kawaxbart@gmail.com>
2021-11-17 04:19:07 +0100

    Remove the assignment instructions (to-do list)

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,13 +57,4 @@ elif option == options[1]:
             answer = translator(text)
             st.success('Tłumaczenie:\n\n' + text + ' -> ' + answer[0]['translation_text'])
 
-st.subheader('Zadanie do wykonania')
-st.write('Wykorzystaj Huggin Face do stworzenia swojej własnej aplikacji tłumaczącej tekst z języka angielskiego na język niemiecki. Zmodyfikuj powyższy kod dodając do niego kolejną opcję, tj. tłumaczenie tekstu. Informacje potrzebne do zmodyfikowania kodu znajdziesz na stronie Huggin Face - https://huggingface.co/transformers/usage.html')
-st.write('✅ ~~Dodaj właściwy tytuł do swojej aplikacji, może jakieś grafiki?~~')
-st.write('✅ ~~Dodaj krótką instrukcję i napisz do czego służy aplikacja~~')
-st.write('✅ ~~Wpłyń na user experience, dodaj informacje o ładowaniu, sukcesie, błędzie, itd.~~')
-st.write('✅ ~~Na końcu umieść swój numer indeksu~~')
-st.write('✅ ~~Stwórz nowe repozytorium na GitHub, dodaj do niego swoją aplikację, plik z wymaganiami (requirements.txt)~~')
-st.write('✅ ~~Udostępnij stworzoną przez siebie aplikację (https://share.streamlit.io) a link prześlij do prowadzącego~~')
-
 st.caption("@ Bartłomiej Kawa, s18581")
